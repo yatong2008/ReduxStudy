84961713f77c8eb859c6ea32910ad800eb27421a
Desktop-ZHOU <yatong.zhou@gmail.com>
2017-09-19 00:52:30 +1000

    Initial App Structure

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,12 @@
+import React, {ReactTypes} from 'react';
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
