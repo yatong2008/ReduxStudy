1ff55a38ab250f96e24772e8ea4d4a78cd7b568e
Desktop-ZHOU <yatong.zhou@gmail.com>
2017-09-29 22:27:07 +1000

    Create Save Course Thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,13 +1,33 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
+
 export function loadCoursesSuccess(courses) {
   return {type: types.LOAD_COURSES_SUCCESS, courses};
+}
+
+export function createCoursesSuccess(courses) {
+  return {type: types.CREATE_COURSES_SUCCESS, course};
+}
+
+export function updateCoursesSuccess(course) {
+  return {type: types.UPDATE_COURSES_SUCCESS, course};
 }
 
 export function loadCourses() {
   return function (dispatch) {
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
+
+export function saveCourse() {
+  return function (dispatch, getState) {
+    return courseApi.saveCourse(course).then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+        dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
       throw(error);
     });
